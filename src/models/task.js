d1daa3933d858d9f338f7f6ef0d98b7adb15b188
Riyansh Pal <riyansh2502@gmail.com>
2020-09-05 01:38:03 +0530

    proper alignment

--- src/models/task.js
+++ src/models/task.js
@@ -1,23 +1,24 @@
-const mongoose=require('mongoose')
+const mongoose = require('mongoose')
 
-const taskSchema= new mongoose.Schema({
-    description:{
-        type:String,
-        required:true,
-        trim:true
+const taskSchema = new mongoose.Schema({
+    description: {
+        type: String,
+        required: true,
+        trim: true
     },
-    completed:{
-        type:Boolean,
-        default:false
+    completed: {
+        type: Boolean,
+        default: false
     },
-    owner:{
-        type:mongoose.Schema.Types.ObjectId,
-        required:true,
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
         ref: 'User'
-    } 
-},{
-    timestamps:true
+    }
+}, {
+    timestamps: true
 })
-const Task=mongoose.model('Task',taskSchema)
 
-module.exports=Task
+const Task = mongoose.model('Task', taskSchema)
+
+module.exports = Task
--- src/models/user.js
+++ src/models/user.js
@@ -1,113 +1,121 @@
-const mongoose=require('mongoose')
-const validator=require('validator')
-const bcrypt=require('bcryptjs')
-const jwt=require('jsonwebtoken')
-const Task =require('./task')
+const mongoose = require('mongoose')
+const validator = require('validator')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const Task = require('./task')
 
-const userSchema= new mongoose.Schema({
-    name:{
-        type:String,
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
         required: true,
-        trim:true
+        trim: true
     },
-    email:{
-        type:String,
-        unique:true,
-        required:true,
-        trim:true,
-        lowercase:true,
-        validate(value){
-            if(!validator.isEmail(value)){
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
                 throw new Error('Email is invalid')
             }
         }
     },
-    password:{
+    password: {
         type: String,
-        required:true,
-        minlength:7,
-        trim:true,
-        validate(value){
-            if(value.toLowerCase().includes('password')){
+        required: true,
+        minlength: 7,
+        trim: true,
+        validate(value) {
+            if (value.toLowerCase().includes('password')) {
                 throw new Error('Password cannot contain "password"')
             }
         }
-
     },
-    age:{
-        type:Number,
-        default:0,
-        validate(value){
-            if(value<0){
-                throw new Error('Age must be positive')
+    age: {
+        type: Number,
+        default: 0,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Age must be a postive number')
             }
         }
     },
     tokens: [{
-        token:{
-            type:String,
-            required:true
+        token: {
+            type: String,
+            required: true
         }
     }],
-    avatar:{
-        type:Buffer
+    avatar: {
+        type: Buffer
     }
-
 }, {
-    timestamps:true
+    timestamps: true
 })
 
-userSchema.virtual('tasks',{
-    ref:'Task',
-    localField:'_id',
-    foreignField:'owner'
+userSchema.virtual('tasks', {
+    ref: 'Task',
+    localField: '_id',
+    foreignField: 'owner'
 })
 
-userSchema.methods.toJSON=function(){
-    const user=this
-    const userObject=user.toObject()
+userSchema.methods.toJSON = function () {
+    const user = this
+    const userObject = user.toObject()
+
     delete userObject.password
     delete userObject.tokens
     delete userObject.avatar
+
     return userObject
 }
-userSchema.methods.generateAuthToken= async function(){
-    const user=this
-    const token= jwt.sign({_id:user._id.toString()},process.env.JWT_SECRET)
 
-    user.tokens=user.tokens.concat({token})
+userSchema.methods.generateAuthToken = async function () {
+    const user = this
+    const token = jwt.sign({ _id: user._id.toString() }, process.env.JWT_SECRET)
+
+    user.tokens = user.tokens.concat({ token })
     await user.save()
-    return token
 
+    return token
 }
 
-userSchema.statics.findByCredentials= async(email,password)=>{
-    const user =await User.findOne({email})
-    if(!user){
+userSchema.statics.findByCredentials = async (email, password) => {
+    const user = await User.findOne({ email })
+
+    if (!user) {
         throw new Error('Unable to login')
     }
-    const isMatch =await bcrypt.compare(password,user.password)
-    if(!isMatch){
-        throw new Error ('Unable to login')
+
+    const isMatch = await bcrypt.compare(password, user.password)
+
+    if (!isMatch) {
+        throw new Error('Unable to login')
     }
+
     return user
 }
 
-userSchema.pre('save',async function(next){
-    const user=this
-    if(user.isModified('password')){
-        user.password =await bcrypt.hash(user.password,8)
+// Hash the plain text password before saving
+userSchema.pre('save', async function (next) {
+    const user = this
+
+    if (user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 8)
     }
 
     next()
 })
 
-userSchema.pre('remove', async function (next){
-    const  user=this
-    await Task.deleteMany({owner:user._id})
+// Delete user tasks when user is removed
+userSchema.pre('remove', async function (next) {
+    const user = this
+    await Task.deleteMany({ owner: user._id })
     next()
 })
 
-const User=mongoose.model('User',userSchema)
+const User = mongoose.model('User', userSchema)
 
-module.exports=User
+module.exports = User
