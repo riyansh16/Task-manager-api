b236b872a8c811ca4223cd3f568e6263d2a46918
Riyansh Pal <riyansh2502@gmail.com>
2020-09-09 20:59:00 +0530

    link html file

--- src/index.js
+++ src/index.js
@@ -10,6 +10,10 @@ app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
 
+app.get('/',(req,res)=>{
+    res.sendFile('./public/front.html',{root:__dirname})
+})
+
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
